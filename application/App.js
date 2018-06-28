649fe6baa9e92ce7c52b8032852961e5fbe7e987
to4-yanagi <to4.yanagi@gmail.com>
2018-06-28 21:17:19 +0900

    componentsフォルダを分割

--- application/App.js
+++ application/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'expo';
+import Map from './src/components/Map'
 
 export default class App extends React.Component {
   render() {
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <Map />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
