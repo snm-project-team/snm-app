8dffeb827d1090710f4a7549e9e91d111f0f7d34
to4-yanagi <to4.yanagi@gmail.com>
2018-09-22 09:50:50 +0900

    マーカーを仮で出す

--- src/components/main/map.js
+++ src/components/main/map.js
@@ -12,7 +12,15 @@ export default class Map extends Component {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <MapView.Marker
+          coordinate={{ latitude: 37.73538, longitude: -122.4324 }}
+          title="marker.title"
+          description="desss"
+          // sagaのイベント、詳細情報をコール、独自コンポネント呼びだし、座標どうするかが問題?
+          onPress={() => console.log('hey')}
+        />
+      </MapView>
     );
   }
 }
